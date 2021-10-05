c5e76e3ccc48a7c5e02f26897687a0224b28a1f8
Pedro Henrique <pedro@lifelink.com.br>
2021-10-05 11:30:23 -0300

    feat: fill the regions array

---
+++ index.js
@@ -0,0 +1,25 @@
+// The regions that have a connection are: Norte -> Nordeste, Sudeste -> Centro Oeste, Sudeste -> Sul
+const regionsInfos = require('./regionsConnecteds.json');
+const allRegions = regionsInfos.regions;
+
+// All regions must to be in the arrival and departure array
+const arrival = [];
+const departure = [];
+
+regionsInfos.regionsConnecteds.forEach((region) => {
+  arrival.push(region.from);
+  departure.push(region.to);
+})
+
+allRegions.forEach((region) => {
+  isOnArrival = arrival.find((element) => element === region);
+  isOnDeparture = departure.find((element) => element === region);
+  
+  if (!isOnArrival(region)) {
+
+  }
+})
+
+console.log(arrival);
+console.log(departure);
+console.log(isOnArrivalAndDeparture('Nordeste'));
